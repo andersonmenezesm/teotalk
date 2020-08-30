9c680cb4a97bc5f77c8a0957763195d9b2c2e516
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-08-30 01:56:04 -0300

    Error (Looking for error in db creation)

---
+++ backend/src/database/migrations/1598759467455-CreatePodcasts.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreatePodcasts1598759467455 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'podcasts',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+          {
+            name: 'audiofile',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('podcasts');
+    }
+
+}
