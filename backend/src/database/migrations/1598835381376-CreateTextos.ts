ae65d572c691aa1e07a35e04c4a1ba644c645196
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-08-30 23:29:07 -0300

    created text database

---
+++ backend/src/database/migrations/1598835381376-CreateTextos.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTextos1598835381376 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'texts',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            //default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'author',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'content',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+          {
+            name: 'secondauthor',
+            type: 'varchar',
+            isNullable: true,
+          }
+        ]
+      })
+    )
+    }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('texts');
+  }
+
+
+}
